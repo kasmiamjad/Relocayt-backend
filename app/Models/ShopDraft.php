a829e3fbf92fa796814b25c652bfa75f4f0df7c7
kasmiamjad <kasmi.amjad@gmail.com>
2025-07-08 12:40:58 +0530

    fix(shopdraft):update shop draft model

--- app/Models/ShopDraft.php
+++ app/Models/ShopDraft.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ShopDraft extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'data', 'step'];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
 }
