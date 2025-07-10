8af476e672ffd5d8230212972b8d61056194f9ae
kasmiamjad <kasmi.amjad@gmail.com>
2025-07-10 18:37:38 +0530

    fix(servicemastercontroller):apdate db query

--- app/Http/Controllers/API/v1/Dashboard/Master/ServiceMasterController.php
+++ app/Http/Controllers/API/v1/Dashboard/Master/ServiceMasterController.php
@@ -14,6 +14,7 @@ use App\Services\ServiceMasterService\ServiceMasterService;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Repositories\ServiceMasterRepository\ServiceMasterRepository;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class ServiceMasterController extends MasterBaseController
 {
