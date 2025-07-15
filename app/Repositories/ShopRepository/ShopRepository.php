0e56069d003ffb0874f006349a8fab0ca226bab6
kasmiamjad <kasmi.amjad@gmail.com>
2025-07-15 15:07:14 +0530

    fix(shop): remove hidden limits and fetch all online services correctly

--- app/Repositories/ShopRepository/ShopRepository.php
+++ app/Repositories/ShopRepository/ShopRepository.php
@@ -93,40 +93,40 @@ class  ShopRepository extends CoreRepository
         $latitude  = data_get($filter, 'address.latitude');
         $longitude = data_get($filter, 'address.longitude');
 
-        return $shop
-    ->whereHas('services', fn($q) => $q->where('type', 'online'))
-    ->with([
-        'translation' => fn($q) => $q->where('locale', $this->language),
-        'services' => fn($q) => $q->where('type', 'online'),
-        'services.translation' => fn($q) => $q->where('locale', $this->language),
-        'services.serviceExtras.translation' => fn($q) => $q->where('locale', $this->language)
-                                                           ->select('id', 'service_extra_id', 'title', 'locale'),
-        'closedDates',
-        'workingDays',
-    ])
-    ->select([
-        'id',
-        'uuid',
-        'slug',
-        'logo_img',
-        'background_img',
-        'status',
-        'type',
-        'delivery_time',
-        'delivery_type',
-        'open',
-        'visibility',
-        'verify',
-        'r_count',
-        'r_avg',
-        'min_price',
-        'max_price',
-        'service_min_price',
-        'service_max_price',
-        'latitude',
-        'longitude',
-    ])
-    ->paginate($filter['perPage'] ?? 10);
+       return $shop
+        ->whereHas('services', fn($q) => $q->where('type', 'online'))
+        ->with([
+            'translation' => fn($q) => $q->where('locale', $this->language),
+            'services' => fn($q) => $q->where('type', 'online'),
+            'services.translation' => fn($q) => $q->where('locale', $this->language),
+            'services.serviceExtras.translation' => fn($q) => $q->where('locale', $this->language)
+                                                            ->select('id', 'service_extra_id', 'title', 'locale'),
+            'closedDates',
+            'workingDays',
+        ])
+        ->select([
+            'id',
+            'uuid',
+            'slug',
+            'logo_img',
+            'background_img',
+            'status',
+            'type',
+            'delivery_time',
+            'delivery_type',
+            'open',
+            'visibility',
+            'verify',
+            'r_count',
+            'r_avg',
+            'min_price',
+            'max_price',
+            'service_min_price',
+            'service_max_price',
+            'latitude',
+            'longitude',
+        ])
+        ->paginate($filter['perPage'] ?? 10);
 
 
     }
