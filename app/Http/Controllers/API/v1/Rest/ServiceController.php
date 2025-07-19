001bb914e7b3f13a383bb67e0d8b9e3c4ec8ec6c
kasmiamjad <kasmi.amjad@gmail.com>
2025-07-19 13:44:05 +0530

    refactor(service): log added

--- app/Http/Controllers/API/v1/Rest/ServiceController.php
+++ app/Http/Controllers/API/v1/Rest/ServiceController.php
@@ -29,6 +29,9 @@ class ServiceController extends RestBaseController
     {
         Log::info('Reached ServiceController@index');
         $models = $this->repository->paginate($request->merge(['has_master' => true])->all());
+                Log::info('First service content:', [
+            'content' => $models->first()->content
+        ]);
 
         return ServiceResource::collection($models);
     }
